7788e0206d2dae66dbb265bc9aab338ecfc7e49f
Voicu Andrei <andrei.voicu@visma.com>
2025-03-16 11:46:09 +0200

    work on button

--- src/core/components/GaButton.stories.ts
+++ src/core/components/GaButton.stories.ts
@@ -3,14 +3,31 @@ import GaButton from '@/core/components/GaButton.vue'
 
 const meta = {
   component: GaButton,
-  title: 'Core/GaButton',
+  title: 'Molecules/Button',
+  parameters: { layout: 'centered' },
+  decorators: [
+    (story) => ({
+      components: { story },
+      template: '<div class="space-x-2"><story /></div>',
+    }),
+  ],
   tags: ['autodocs'],
 } satisfies Meta<typeof GaButton>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = { args: { default: 'Button' } }
+export const Default = {
+  args: {},
+  render: () => ({
+    components: { GaButton },
+    template: `
+      <ga-button>Button</ga-button>
+      <ga-button disabled>Button</ga-button>
+    `,
+  }),
+}
+
 export const Primary: Story = { args: { default: 'Button', primary: true } }
 export const Transparent: Story = { args: { default: 'Button', transparent: true } }
 
