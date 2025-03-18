6b9093d64822beb810eea8f11d6e939941ccc856
Voicu Andrei <andrei.voicu@visma.com>
2025-03-18 10:27:02 +0200

    upload for build

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { purgeCSS } from './src/plugins/purgeCss'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), purgeCSS({ variables: true })],
-  base: process.env.NODE_ENV === 'production' ? '/v-StoryApp/' : '/',
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+export default defineConfig(({ mode }) => {
+  const plugins = [vue(), vueDevTools()]
+  if (mode !== 'test') plugins.push(purgeCSS({ variables: true }))
+
+  return {
+    plugins: plugins,
+    base: process.env.NODE_ENV === 'production' ? '/v-StoryApp/' : '/',
+    resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  }
 })
