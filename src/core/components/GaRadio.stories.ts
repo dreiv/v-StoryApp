df7417e2aa0776850869fa3b19e53e2811fe895e
Voicu Andrei <andrei.voicu@visma.com>
2025-03-24 19:16:16 +0200

    radio

---
+++ src/core/components/GaRadio.stories.ts
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import GaRadio, { type RadioProps } from './GaRadio.vue'
+
+const meta = {
+  component: GaRadio,
+  title: 'Molecules/Radio',
+  parameters: { layout: 'centered' },
+  decorators: [
+    (story) => ({
+      components: { story },
+      template: `<div :style="{display:'flex',flexWrap:'wrap',alignItems:'center',gap:'8px',padding:'1rem'}"><story /></div>`,
+    }),
+  ],
+  tags: ['autodocs'],
+  argTypes: {
+    modelValue: { control: 'boolean', description: 'The checked state of the radio input.' },
+    error: { control: 'boolean', description: 'Indicates an error state for the radio input.' },
+    errorMessage: {
+      control: 'text',
+      description: 'The error message to display when the radio input is in error state.',
+    },
+    default: { control: 'text', description: 'The label text for the radio input.' },
+  },
+} as Meta<typeof GaRadio>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+interface RadioVariation {
+  name: 'Default' | 'Checked' | 'Indeterminate' | 'Error' | 'ErrorChecked' | 'ErrorIndeterminate'
+  props: RadioProps & { default?: string; checked?: boolean; disabled?: boolean }
+}
+
+const variations: RadioVariation[] = [
+  { name: 'Default', props: { checked: false } },
+  { name: 'Checked', props: { checked: true } },
+  { name: 'Error', props: { error: true, errorMessage: 'Something happened', checked: false } },
+  {
+    name: 'ErrorChecked',
+    props: { error: true, errorMessage: 'Something happened', checked: true },
+  },
+]
+
+const createStory = (variation: RadioVariation): Story => ({
+  args: variation.props,
+  render: (args) => ({
+    components: { GaRadio },
+    template: `
+      <ga-radio v-bind="args" />
+      <ga-radio v-bind="args" disabled />
+      <ga-radio v-bind="args">
+        {{ args.default || 'With Label' }}
+      </ga-radio>
+    `,
+    setup: () => ({ args }),
+  }),
+})
+
+const stories: { [key: string]: Story } = {}
+variations.forEach((variation) => {
+  stories[variation.name] = createStory(variation)
+})
+
+export const Default: Story = stories.Default
+export const Checked: Story = stories.Checked
+export const Error: Story = stories.Error
+export const ErrorChecked: Story = stories.ErrorChecked
---
+++ src/core/components/GaRadio.vue
@@ -0,0 +1,150 @@
+<script setup lang="ts">
+import { computed, useCssModule } from 'vue'
+
+export interface RadioProps {
+  modelValue?: boolean
+  error?: boolean
+  errorMessage?: string
+  label?: string
+}
+
+defineOptions({ inheritAttrs: false })
+const { error, errorMessage } = defineProps<RadioProps>()
+const style = useCssModule()
+const model = defineModel()
+
+const classes = computed(() => [style.radio, { [style.error]: error }])
+const aria = computed(() => ({
+  'aria-invalid': error ? true : undefined,
+  'aria-errormessage': errorMessage ?? undefined,
+}))
+</script>
+
+<template>
+  <label :class="classes" v-bind="aria">
+    <input type="radio" :class="$style.native" v-model="model" v-bind="$attrs" />
+    <div :class="$style.marker" />
+
+    <span :class="$style.label">
+      <slot>{{ label }}</slot>
+    </span>
+  </label>
+</template>
+
+<style module>
+.radio {
+  display: inline-flex;
+  position: relative;
+  gap: var(--ga-size-8);
+  user-select: none;
+}
+
+.native,
+.marker {
+  margin: var(--ga-size-2);
+  width: var(--ga-size-16);
+  height: var(--ga-size-16);
+}
+
+.marker {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  border: 2px solid var(--ga-color-border-action);
+  border-radius: var(--ga-radius-round);
+
+  pointer-events: none;
+  color: var(--ga-color-icon-on-primary);
+}
+
+.native {
+  flex-shrink: 0;
+  opacity: 0;
+  cursor: pointer;
+
+  &:focus-visible + .marker {
+    outline: 2px solid var(--ga-color-border-focus);
+    outline-offset: 2px;
+  }
+
+  &:hover:enabled {
+    + .marker {
+      border-color: var(--ga-color-border-action-hover);
+    }
+
+    &:not(:checked) + .marker {
+      background-color: var(--ga-color-surface-action-hover-2);
+    }
+  }
+
+  &:checked + .marker {
+    background-color: var(--ga-color-surface-action);
+
+    &::after {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+
+      border-radius: var(--ga-radius-round);
+      background-color: var(--ga-color-icon-on-primary);
+
+      width: var(--ga-size-4);
+      height: var(--ga-size-4);
+      content: '';
+    }
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+
+    + .marker {
+      border-color: var(--ga-color-border-disabled);
+      background-color: var(--ga-color-surface-disabled);
+      color: var(--ga-color-icon-on-disabled);
+    }
+
+    &:checked {
+      + .marker::after {
+        background-color: var(--ga-color-icon-on-disabled);
+      }
+      ~ .label {
+        color: var(--ga-color-text-disable-selected);
+      }
+    }
+
+    ~ .label {
+      color: var(--ga-color-text-disabled);
+    }
+  }
+}
+
+.error {
+  > .marker {
+    border-color: var(--ga-color-border-error);
+    background-color: var(--ga-color-surface-error);
+    color: var(--ga-color-icon-error);
+  }
+
+  > .native {
+    &:checked:enabled + .marker {
+      background-color: var(--ga-color-icon-error);
+    }
+
+    &:hover:enabled + .marker {
+      border-color: var(--ga-color-red-70);
+    }
+  }
+}
+
+.label {
+  min-height: var(--ga-size-16);
+  font-size: var(--text-md);
+  line-height: var(--text-md--line-height);
+
+  &:empty {
+    display: none;
+  }
+}
+</style>
