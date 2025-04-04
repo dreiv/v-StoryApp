c45f336b2a3406ed81886b6687cecf0659ea6b9b
Voicu Andrei <andrei.voicu@visma.com>
2025-04-04 13:10:47 +0300

    work on tooltip

--- src/core/components/GaTooltip.vue
+++ src/core/components/GaTooltip.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTemplateRef } from 'vue'
+import { useFloating } from '../composables/useFloating'
+
+const reference = useTemplateRef('reference')
+const floating = useTemplateRef('floating')
+const { floatingStyles } = useFloating(reference, floating)
+</script>
 
 <template>
-  <div class="ga-tooltip">
-    <slot></slot>
-  </div>
+  <span ref="reference" :class="$style.reference">
+    <slot />
+  </span>
+
+  <teleport to="body">
+    <div ref="floating" :style="floatingStyles">Tooltip</div>
+  </teleport>
 </template>
+
+<style module>
+.reference {
+  /* display: contents; */
+}
+</style>
