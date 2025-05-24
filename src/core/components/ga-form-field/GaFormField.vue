baf468996c3e3791d085ee4a3d1829202e3ce8cc
Voicu Andrei <andrei.voicu@visma.com>
2025-05-24 23:11:14 +0300

    work on form field

--- src/core/components/ga-form-field/GaFormField.vue
+++ src/core/components/ga-form-field/GaFormField.vue
@@ -4,13 +4,15 @@ const { label } = defineProps<{ label?: string }>()
 
 <template>
   <div :class="$style.formField">
-    <slot name="label">
-      <label :class="$style.label">{{ label }}</label>
-    </slot>
+    <label :class="$style.label" v-if="$slots.label || label">
+      <slot name="label">{{ label }}</slot>
+    </label>
 
     <slot />
 
-    <slot name="info" />
+    <div :class="$style.info" v-if="$slots.info">
+      <slot name="info" />
+    </div>
   </div>
 </template>
 
@@ -23,5 +25,20 @@ const { label } = defineProps<{ label?: string }>()
 
 .label {
   display: inline-flex;
+  gap: var(--ga-size-spacing-02);
+  padding: 0 var(--ga-size-spacing-02);
+
+  width: fit-content;
+  font-weight: var(--ga-font-weight-medium);
+}
+
+.info {
+  display: flex;
+  align-items: center;
+  gap: var(--ga-size-spacing-02);
+
+  font-size: var(--ga-text-xs-font-size);
+  line-height: var(--ga-text-xs-line-height);
+  letter-spacing: var(--ga-text-xs-tracking);
 }
 </style>
